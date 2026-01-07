cf279423c8c891087da17d00d39446aa6f55292a
J2085isa <alvarezramirezisaias11@gmail.com>
2026-01-07 16:49:26 -0600

    Actualizar android.py

--- Android/android.py
+++ Android/android.py
@@ -848,3 +848,21 @@ if __name__ == "__main__":
     print("> TIKTOK:\n", pub_tiktok)
 
     print("\n=== PROCESO COMPLETADO ===")
+/*
+ * Copyright 2026 Alvarez Trucking 1, S.A. de C.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alvareztrucking1.app.data
+// ... resto del código
